1aeb4be6fc31a3855386472dee63c6f0cbc1260a
Louis Bettens <louis@bettens.info>
2022-03-26 11:56:22 +0100

    add a fetch program

---
+++ fetch.c
@@ -0,0 +1,15 @@
+#include "libc/stdio/stdio.h"
+#include "libc/calls/calls.h"
+#include "libc/log/log.h"
+
+int main() {
+	struct utsname info;
+	if (uname(&info) == 0)
+		err(1, "uname");
+
+	printf("Hostname:\t%s\n", info.nodename);
+	printf("Arch:    \t%s\n", info.machine);
+	printf("OS:      \t%s %s\n", info.sysname, info.release);
+	printf("Info:    \t%s\n", info.version);
+	(void)info.domainname;
+}
